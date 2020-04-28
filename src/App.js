cd6a9f35aee99439c0bce4aced0b9ca63aac6ddb
AJ <alexanderjdiaz89@gmail.com>
2020-04-28 11:03:44 -0600

    add drink form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import DrinkForm from "./Components/DrinkForm"
 
 class App extends Component{
 
   render() {
     return (
       <div className="App">
+        <DrinkForm/>
         
       </div>
     );
---
+++ src/Components/DrinkForm.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function DrinkForm() {
+    return <h1>I'm a H1</h1>
+}
