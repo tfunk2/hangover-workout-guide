eb0fd585830216780b78f98d397651843e227724
AJ <alexanderjdiaz89@gmail.com>
2020-04-28 11:50:39 -0600

    add form styling

--- src/Components/DrinkForm.js
+++ src/Components/DrinkForm.js
@@ -1,5 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function DrinkForm() {
-    return <h1>I'm a H1</h1>
+export default class DrinkForm extends Component {
+    
+    
+    render(){
+        return (
+            <>
+                <form className="drink-form">
+                    <select id="drink-select" class="drink-dropdown" name="alcoholic-beverage">
+                        <option value="#">Light Beer</option>
+                        <option value="#">Regular Beer</option>
+                        <option value="#">Wine</option>
+                        <option value="#">Spirits</option>
+                    </select>
+                    <label id="drink-label" for="quantity">number of drinks:  </label>
+                    <input id="drink-quantity" type="number" min="1" max="12" name="quantity"></input>
+                    <input id="drink-submit" type="submit" value="submit"></input>
+                </form>
+            </>
+        )
+    }
 }
