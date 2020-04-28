0db6f5f3cb9001c50ef45d50db59f8166c457f3b
tfunk2 <tyler.j.funk2@gmail.com>
2020-04-28 15:02:46 -0600

    create minimum viable product

--- src/components/WorkoutSuggestion.js
+++ src/components/WorkoutSuggestion.js
@@ -4,7 +4,7 @@ export default function WorkoutSuggestion(props) {
     return (
         <li className="workout-suggestion">
             <h2>{props.workout.name}</h2>
-            <p>Calories this will burn: {props.workout.calories}</p>
+            <p>Calories this will burn: {props.workout.calories} / hour</p>
         </li>
     )
 }
